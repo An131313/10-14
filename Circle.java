7a121ce92b3c51f5592ab764b1cdd09c16408fda
An131313 <71447680+An131313@users.noreply.github.com>
2020-12-03 12:30:35 +0200

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,28 @@
+package circle;
+
+public class Circle {
+	private double radius;
+	
+	public Circle() {
+		radius = 1;
+	}
+	
+	public Circle(double newRadius) {
+		radius = newRadius;
+	}
+	
+	public double getArea() {
+		return Math.PI * radius * radius;
+	}
+	
+	public double getPerimeter() {
+		return Math.PI * radius * 2;
+	}
+	
+	public void setRadius(double newRadius) {
+		this.radius = newRadius;
+	}
+	public double getRadius() {
+		return radius;
+	}
+}
